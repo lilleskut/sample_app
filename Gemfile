66f534628941091fd103a929b67b62b6d6323322
Jens Tobiska <jtobiska@gmail.com>
2012-07-07 23:28:57 +0200

    Add guard gem

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.6'
 group :development, :test  do
 	gem 'sqlite3', '1.3.5'
 	gem 'rspec-rails', '2.10.0'
+	gem 'guard-rspec', '0.5.5'
 end
 
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
@@ -11,7 +12,7 @@ describe "Static pages" do
 
 	it "should have the title 'Home'" do
 	  visit '/static_pages/home'
-	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+	  page.should have_selector('title', :text => "#{base_title} | Home")
 	end
   end
 
@@ -24,7 +25,7 @@ describe "Static pages" do
 
 	it "should have the title 'Help'" do
 	  visit '/static_pages/help'
-	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+	  page.should have_selector('title', :text => "#{base_title} | Help")
 	end
 
   end
@@ -38,7 +39,7 @@ describe "Static pages" do
 
 	it "should have the title 'About Us'" do
 	  visit '/static_pages/about'
-	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+	  page.should have_selector('title', :text => "#{base_title} | About Us")
 	end
   end
 
@@ -51,7 +52,7 @@ describe "Static pages" do
 
 	it "should have the title 'Contact'" do
 	  visit '/static_pages/contact'
-	  page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+	  page.should have_selector('title', :text => "#{base_title} | Contact")
 	end
   end
 end
