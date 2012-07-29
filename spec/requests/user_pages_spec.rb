f21948689ebc4c34f704242ff7cb89475470f059
Jens Tobiska <jtobiska@gmail.com>
2012-07-29 09:48:36 +0200

    Finish user edit, update, index and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,7 +11,7 @@ describe "User pages" do
 	before(:all) { 30.times { FactoryGirl.create(:user) } }
 	after(:all) { User.delete_all }
 
-	before(:each) do
+	before do
 	  sign_in user
 	  visit users_path
 	end
@@ -27,6 +27,24 @@ describe "User pages" do
 	    User.paginate(page: 1).each do |user|
 		  page.should have_selector('li', text: user.name)
 		end
+	  end
+	end
+
+	describe "delete links" do
+	  it { should_not have_link('delete') }
+	  
+	  describe "as an admin user" do
+		let(:admin) { FactoryGirl.create(:admin) }
+		before do
+		  sign_in admin
+		  visit users_path
+		end
+
+		it { should have_link('delete', href: user_path(User.first)) }
+		it "should be able to delete another user" do
+		  expect { click_link('delete') }.to change(User, :count).by(-1)
+		end
+		it { should_not have_link('delete', href: user_path(admin)) }
 	  end
 	end
   end
