f30011eaf7e8f665780e92fbc4a73cf4cf9c4de5
Jens Tobiska <jtobiska@gmail.com>
2012-08-19 00:04:16 +0200

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,9 +25,16 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  def feed
+    # This is preliminary. see "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 	
 	def create_remember_token
 	  self.remember_token = SecureRandom.urlsafe_base64
 	end
+
+	
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -135,6 +136,7 @@ describe User do
   end
 
   describe "micropost associations" do
+
 	before { @user.save }
 	let!(:older_micropost) do
 	  FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
@@ -153,6 +155,16 @@ describe User do
 	  microposts.each do |micropost|
 		Micropost.find_by_id(micropost.id).should be_nil
 	  end
+	end
+
+	describe "status" do
+	  let(:unfollowed_post) do
+		FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+	  end
+
+	  its(:feed) { should include(newer_micropost) }
+	  its(:feed) { should include(older_micropost) }
+	  its(:feed) { should_not include(unfollowed_post) }
 	end
   end
 end
